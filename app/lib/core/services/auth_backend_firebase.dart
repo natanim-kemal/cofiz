24510ef3a7a87ddb1bebea3caaa125885c57dc73
Natanim-Kemal <kemalnatanim@gmail.com>
2026-08-29 10:47:28 +0300

    test(auth): make AuthBackendFirebase constructible in unit tests

--- app/lib/core/services/auth_backend_firebase.dart
+++ app/lib/core/services/auth_backend_firebase.dart
@@ -1,14 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthBackendFirebase {
-  AuthBackendFirebase({FirebaseAuth? auth}) : _auth = auth ?? FirebaseAuth.instance;
-  final FirebaseAuth _auth;
+  AuthBackendFirebase({FirebaseAuth? auth}) : _auth = auth;
+  final FirebaseAuth? _auth;
+  FirebaseAuth get _resolved => _auth ?? FirebaseAuth.instance;
 
   Future<UserCredential> signInWithCustomToken(String token) {
-    return _auth.signInWithCustomToken(token);
+    return _resolved.signInWithCustomToken(token);
   }
 
-  Future<void> signOut() => _auth.signOut();
+  Future<void> signOut() => _resolved.signOut();
 
-  Stream<User?> get authStateChanges => _auth.authStateChanges();
+  Stream<User?> get authStateChanges => _resolved.authStateChanges();
 }
--- app/test/core/services/auth_backend_firebase_test.dart
+++ app/test/core/services/auth_backend_firebase_test.dart
@@ -2,8 +2,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:cofiz/core/services/auth_backend_firebase.dart';
 
 void main() {
-  test('AuthBackendFirebase is constructible', () {
+  test('AuthBackendFirebase is constructible without injected auth (default)', () {
     final s = AuthBackendFirebase();
     expect(s, isNotNull);
+    expect(s.signInWithCustomToken, isA<Function>());
+    expect(s.signOut, isA<Function>());
   });
 }
